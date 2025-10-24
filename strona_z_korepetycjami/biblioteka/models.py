bf4ba8887c480cd1a5431204d04847792ed4e016
Tymax1410 <tmiskiewicz@st.swps.edu.pl>
2025-10-24 13:31:03 +0200

    lab_04

--- strona_z_korepetycjami/biblioteka/models.py
+++ strona_z_korepetycjami/biblioteka/models.py
@@ -55,3 +55,20 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Stanowisko(models.Model):
+    nazwa = models.CharField(max_length=70, null = False, blank = False)
+    opis = models.TextField(null = True, blank = True) #opcjonalne
+
+class Osoba(models.Model):
+    PLEC_WYBOR = (
+        ("K", "kobieta"),
+        ("M", "mężczyzna"),
+        ("I", "inna")
+    )
+    imie = models.CharField(max_length=50, null = False, blank = False)
+    nazwisko = models.CharField(max_length=100, null = False, blank = False)
+    plec = models.CharField(max_length=1, choices=PLEC_WYBOR, default="I")
+    stanowisko = models.ForeignKey('Stanowisko', on_delete = models.CASCADE)
+
+
