94acc891b4621ee1bd14822d76ae95e1a72f1c7a
Tymax1410 <tmiskiewicz@st.swps.edu.pl>
2025-11-21 13:32:17 +0100

    lab_07

--- strona_z_korep/strona_z_korepetycjami_og/wsgi.py
+++ strona_z_korep/strona_z_korepetycjami_og/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strona_z_korepetycjami.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'strona_z_korepetycjami_og.settings')
 
 application = get_wsgi_application()
