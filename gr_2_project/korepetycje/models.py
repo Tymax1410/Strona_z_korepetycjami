6c9eee7d41235d3b2059155dce5d5089f62f5b6f
Tymax1410 <tmiskiewicz@st.swps.edu.pl>
2026-01-15 19:00:29 +0100

    niedokończony kod w modelach/tworzenie modeli

--- gr_2_project/korepetycje/models.py
+++ gr_2_project/korepetycje/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Przedmiot(models.Model):
+    nazwa = models.CharField(max_length=100)
+    opis = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.nazwa
+
+class Nauczyciel(models.Model):
+    imie = models.CharField(max_length=100)
+    nazwisko = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    przedmioty = models.ManyToManyField(Przedmiot)
+
+class Lekcja(models.Model):
+    data_odbywania = models.DateField()
+    prowadzacy = models.ManyToManyField(Nauczyciel)
+    uczniowie = 
