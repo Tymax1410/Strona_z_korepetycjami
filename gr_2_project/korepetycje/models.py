1482c9f82db20edb3eaf5621c7d23a853871d983
Tymax1410 <tmiskiewicz@st.swps.edu.pl>
2026-01-16 12:51:49 +0100

    pomyślne pierwsze migracje yeah

--- gr_2_project/korepetycje/models.py
+++ gr_2_project/korepetycje/models.py
@@ -11,9 +11,35 @@ class Nauczyciel(models.Model):
     imie = models.CharField(max_length=100)
     nazwisko = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
+    numer_telefonu = models.CharField(max_length=20)
     przedmioty = models.ManyToManyField(Przedmiot)
 
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
+
+class Uczen(models.Model):
+    imie = models.CharField(max_length=100)
+    nazwisko = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    numer_telefonu = models.CharField(max_length=20)
+    przedmioty = models.ManyToManyField(Przedmiot)
+
+    def __str__(self):
+        return f"{self.imie} {self.nazwisko}"
+
 class Lekcja(models.Model):
-    data_odbywania = models.DateField()
-    prowadzacy = models.ManyToManyField(Nauczyciel)
-    uczniowie = 
+    data_odbywania = models.DateTimeField()
+    nauczyciel = models.ForeignKey(Nauczyciel, on_delete=models.CASCADE)
+    uczen = models.ForeignKey(Uczen, on_delete=models.CASCADE)
+
+    class Status(models.TextChoices):
+        PLANOWANA = 'PL', 'Planowana'
+        ODBYTA = 'ODB', 'Odbyta'
+        ODWOLANA = 'ODW', 'Odwołana'
+    
+    status = models.CharField(
+        max_length=3,
+        choices=Status.choices,
+        default=Status.PLANOWANA
+    )
